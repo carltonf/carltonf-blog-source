be917bc3f1894a0dc5842d6f8abf950dadb4b181
Carl Xiong <xiongc05@gmail.com>
2015-07-29 19:53:48 +0800

    Smoother sidebar toggle
    - Search is not in the header but attached after "Quick Search"
    - Two-step showing of sidebar to have smooth transition.

--- scripts/main.js
+++ scripts/main.js
@@ -4,18 +4,25 @@ $(function(){
         $('#page-content').addClass('col-lg-10 col-lg-offset-1');
     })
 
-    $('#sidebar').on('show.bs.collapse', function(){
-        // temporarily make sidebar invisible to avoid undesirable intermediate
-        // display:
-        // TODO the best way is to have a hook that runs BEFORE show action.
-        $('#sidebar').css("visibility", "hidden");
+    // The two steps in showing sidebar is necessary to avoid intermediate
+    // display issue.
+    $('#sidebar').on('show.bs.collapse', function(e){
+        if( $('#page-content').hasClass('col-lg-offset-1') ){
+            $('#page-content').removeClass('col-lg-10 col-lg-offset-1');
+            $('#page-content').addClass("col-lg-9");
+            return e.preventDefault();
+        }
+    })
+
+    // only really show the sidebar after layout updated.
+    $('#page-content').on('transitionend', function(){
+        // TODO a separate class for whether there is a sidebar shown
+        if ( ! $('#page-content').hasClass('col-lg-offset-1') ){
+            $('#sidebar').collapse('show');
+        }
     })
 
     $('#sidebar').on('shown.bs.collapse', function(){
-        $('#page-content').removeClass('col-lg-10 col-lg-offset-1');
-        $('#page-content').addClass("col-lg-9");
-
-        $('#sidebar').css("visibility", "visible");
         // must be in 'shown' event for this to work
         $('#quick-search').focus();
     })
