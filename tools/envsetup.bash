3d4619c5462f29152cfb88203b7706a2e913d2f8
Carl Xiong <xiongc05@gmail.com>
2017-07-15 19:41:15 +0800

    habit/bash: add autocompletion for sub commands
    * subcommands completions are generated from habit itself
      - a clumsy but dynamic approach

--- tools/envsetup.bash
+++ tools/envsetup.bash
@@ -7,3 +7,23 @@ fi
 ## NOTE import more tools BEGIN
 export PATH=$TOP/tools:$TOP/tools/habit:$PATH
 ## END
+
+
+# TODO move this out to separate file
+#
+# Habit Bash acutocompletion
+function __ac_habit {
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    subcmds=$(habit __ac_tool)
+
+    # 'subcmds' only work immediately after `habit`
+    if [[ ${prev} == 'habit' ]] ; then
+        COMPREPLY=( $(compgen -W "${subcmds}" -- ${cur}) )
+        return 0
+    fi
+}
+
+complete  -o bashdefault -o default -o nospace -F __ac_habit habit
